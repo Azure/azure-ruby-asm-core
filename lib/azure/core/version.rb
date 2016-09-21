86905edd031c3111def270b2d9e44d06d36f29a0
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2016-09-21 13:08:32 -0700

    Preparing for azure-core v0.1.5 release (#26)

--- lib/azure/core/version.rb
+++ lib/azure/core/version.rb
@@ -18,7 +18,7 @@ module Azure
     class Version
       MAJOR = 0 unless defined? MAJOR
       MINOR = 1 unless defined? MINOR
-      UPDATE = 4 unless defined? UPDATE
+      UPDATE = 5 unless defined? UPDATE
       PRE = nil unless defined? PRE
 
       class << self
